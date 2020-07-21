2fff960599d4a97df29b1ae8a1d5f0b7742a8593
sungwoo <tjddn5242@gmail.com>
2020-07-21 18:26:49 +0900

    second commit

--- likelionPJ/urls.py
+++ likelionPJ/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import firstapp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', firstapp.views.index, name="index")
 ]
